8e594e6037d3202b8fd9771eab53f6a72e7a76fe
syed <k.syed200367@gmail.com>
2026-05-04 08:43:41 +0530

    commit for creating project

---
+++ main/java/com/payorch/PayorchApplication.java
@@ -0,0 +1,13 @@
+package com.payorch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayorchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayorchApplication.class, args);
+	}
+
+}
---
+++ test/java/com/payorch/PayorchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payorch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayorchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
