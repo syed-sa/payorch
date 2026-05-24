069df5d5eb0f76b5c0508f016d8f951a67908d27
syed <k.syed200367@gmail.com>
2026-05-24 19:25:47 +0530

    commit for  reconcilation worker base setup

--- reconciliation-worker/src/main/java/com/payorch/reconciliation/ReconciliationWorkerApplication.java
+++ reconciliation-worker/src/main/java/com/payorch/reconciliation/ReconciliationWorkerApplication.java
@@ -1,3 +1,15 @@
+package com.payorch.reconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.payorch.ledger.model", "com.payorch.reconciliation.domain"})
+@EnableJpaRepositories(basePackages = {"com.payorch.ledger.repository", "com.payorch.reconciliation.repository"})
 public class ReconciliationWorkerApplication {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(ReconciliationWorkerApplication.class, args);
+    }
 }
