19771debd285a15aac0ffda7f4a4d56028bcd56c
syed <k.syed200367@gmail.com>
2026-06-21 14:44:41 +0530

    commit for removing ledger management

--- core-orchestrator/src/main/java/com/payorch/ledger/repository/LedgerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.payorch.ledger.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.payorch.shared.model.LedgerEntry;
-
-import java.util.UUID;
-
-public interface LedgerRepository extends JpaRepository<LedgerEntry, UUID> {}
--- core-orchestrator/src/main/java/com/payorch/ledger/service/LedgerService.java
+++
@@ -1,7 +0,0 @@
-package com.payorch.ledger.service;
-
-import com.payorch.shared.model.Transaction;
-
-public interface LedgerService {
-    void recordEntry(Transaction transaction);
-}
