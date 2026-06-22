99e08a42c14dff613a81f76393113864e08805d2
syed <k.syed200367@gmail.com>
2026-06-22 09:20:06 +0530

    commit  for addding util class

---
+++ payorch-shared/src/main/java/com/payorch/shared/providers/util/TokenMaskingUtil.java
@@ -0,0 +1,15 @@
+package com.payorch.shared.providers.util;
+
+public final class TokenMaskingUtil {
+
+    private TokenMaskingUtil() {
+    }
+
+    public static String mask(String token) {
+        if (token == null || token.isBlank()) {
+            return "****";
+        }
+        int idx = token.indexOf('_');
+        return idx > 0 ? token.substring(0, idx + 1) + "****" : "****";
+    }
+}
