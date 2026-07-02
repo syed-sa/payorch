506532faa5ea9585ba93691185af456f73341766
syed <k.syed200367@gmail.com>
2026-07-02 09:22:00 +0530

    adding new module providers

---
+++ payorch-providers/src/main/java/com/payorch/providers/factory/PaymentProviderFactory.java
@@ -0,0 +1,24 @@
+package com.payorch.providers.factory;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.payorch.shared.providers.service.PaymentProvider;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentProviderFactory {
+
+    private final List<PaymentProvider> providers;
+
+    public PaymentProvider get(String providerId) {
+        return providers.stream()
+                .filter(p -> p.supports(providerId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Unsupported provider: " + providerId));
+    }
+}
